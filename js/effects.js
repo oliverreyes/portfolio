e4ee0aba3a5affb514e34dd26df5121085a3cb46
Oliver Reyes <oareyes@ucsd.edu>
2017-02-01 17:56:18 -0800

    Completed text animation for landing page

--- js/effects.js
+++ js/effects.js
@@ -1,7 +1,21 @@
-function textChange(newText){
-
-	$("#title").slideUp(800, function(){
-		$(this).text(newText)
-	}).slideDown();
-	setTimeout(textChange("Software Developer"), 800);
+function readyFunc(){
+	var titleList = ["Software Developer", "UCSD Graduate", "Web Developer"]; 	
+	var index = 0;
+	function textChange(textList, i){
+		$("#title")
+			.slideUp(1000)
+			.queue(function(){
+				$(this).text(textList[i]).slideDown(1000);
+				$(this).dequeue();
+			})
+	}
+	setInterval(function(){ 
+		textChange(titleList, index); 
+		if (index < titleList.length-1) {
+			index++;
+		}
+		else {
+			index = 0;
+		}
+	}, 4000);	
 }
