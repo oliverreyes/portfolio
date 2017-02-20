60c1ab53bf5009326e7596fa1efb02b77ece5068
Oliver Reyes <oareyes@ucsd.edu>
2017-02-19 22:39:37 -0800

    Added pieces

--- js/checkers.js
+++ js/checkers.js
@@ -1,20 +1,31 @@
-
+// create board
 function setBoard(){
 	var nSpaces = 64;
 	var $board = $(".board");
-	for (var i = 0; i < nSpaces; i++) {
+	var nBlack = 0;
+	for (var i = 0; i < nSpaces; i++){
 		var colorIndex = Math.floor(i / 8 + i) % 2;
-		if (colorIndex == 0) {
-			$board.append("<div class='white' id=" + i +"></div>");
+		if (colorIndex == 0){
+			$board.append("<div class='white'></div>");
 		}
 		else {
-			$board.append("<div class='black' id=" + i +"></div>");
+			$board.append("<div class='black'><div id='" + nBlack +"'></div></div>");
+			nBlack++;
 		}
 	}
 }
 
-function setPieces(canvas){
-
+// create pieces
+function setPieces(){
+	for (var i = 0; i < 24; i++){
+		if (i < 12){
+			$("#" + i).addClass("whitePiece");
+		}
+		else {
+			$("#" + (i+8)).addClass("redPiece");
+		} 
+	}
 }
 
 setBoard();
+setPieces();
