53c829603819575aa082d8636cf1740b31b40c9a
Oliver Reyes <oareyes@ucsd.edu>
2017-02-23 11:56:58 -0800

    Checkers

--- js/checkers.js
+++ js/checkers.js
@@ -1,11 +1,19 @@
+
 // create board
 function setBoard(){
 	var nSpaces = 64;
 	var $board = $(".board");
 	var nBlack = 0;
+	window.boardArray = [];
+	// create 2D array
+	for (var j = 0; j < 8; j++){
+		window.boardArray[j] = []; 
+	}
+
 	for (var i = 0; i < nSpaces; i++){
-		var colorIndex = Math.floor(i / 8 + i) % 2;
-		if (colorIndex == 0){
+		var num = Math.floor(i / 8 + i);
+		var isEven =  num % 2;
+		if (isEven == 0){
 			$board.append("<div class='white'></div>");
 		}
 		else {
@@ -13,6 +21,7 @@ function setBoard(){
 			nBlack++;
 		}
 	}
+	
 }
 
 // create pieces
@@ -25,6 +34,30 @@ function setPieces(){
 			$("#" + (i+8)).addClass("redPiece");
 		} 
 	}
+
+	for (var r = 0; r < 8; r++){
+		for (var c = 0; c < 8; c+=2){
+			if (r == 0 || r == 2){
+				window.boardArray[r][c+1] = 0;
+			}
+			else if (r == 1){
+				window.boardArray[r][c] = 0;
+			}
+			else if (r == 5 || r == 7){
+				window.boardArray[r][c] = 1;
+			}
+			else if (r == 6){
+				window.boardArray[r][c+1] = 1;
+			}
+			
+		}
+	}
+	console.log(window.boardArray);
+	
+}
+
+function getCoord(){
+
 }
 
 setBoard();
