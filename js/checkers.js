a00c6c3cee64a87ba2b764774046e68de5a44e54
Oliver Reyes <oareyes@ucsd.edu>
2017-03-01 12:06:17 -0800

    Finished highlight on click and available single moves

--- js/checkers.js
+++ js/checkers.js
@@ -1,23 +1,21 @@
 var white_count = 0;
 var red_count = 0;
 // Create global mapping
-var globalArray = [];
+var globalArray = []; // index is visual board number, value is coords; use indexOf
 var coordArray = mapCoord(globalArray);
-var myMap = new Map(coordArray);
-console.log(myMap);
 
 
 function main(){
 	setBoard();
 	setPieces();
-	// CLICK NOT WORKING, ATTR IS UNDEFINED
 }
 
 // Checker piece object
-function Checker(team, x, y){
+function Checker(team, x, y, isKing){
 	this.team = team;
 	this.x = x;
 	this.y = y;
+	this.isKing = isKing;
 }
 
 // Create board
@@ -53,19 +51,19 @@ function setPieces(){
 	for (var r = 0; r < 8; r++){
 		for (var c = 0; c < 8; c+=2){
 			if (r == 0 || r == 2){
-				window.boardArray[r][c+1] = new Checker("White", c+1, r); 
+				window.boardArray[r][c+1] = new Checker("White", c+1, r, false); 
 				white_count++;
 			}
 			else if (r == 1){
-				window.boardArray[r][c] = new Checker("White", c, r);
+				window.boardArray[r][c] = new Checker("White", c, r, false);
 				white_count++;
 			}
 			else if (r == 5 || r == 7){
-				window.boardArray[r][c] = new Checker("Red", c, r);
+				window.boardArray[r][c] = new Checker("Red", c, r, false);
 				red_count++;
 			}
 			else if (r == 6){
-				window.boardArray[r][c+1] = new Checker("Red", c+1, r);
+				window.boardArray[r][c+1] = new Checker("Red", c+1, r, false);
 				red_count++;
 			}
 		}
@@ -82,36 +80,89 @@ function setPieces(){
 	console.log(window.boardArray);	
 }
 
+// Map index of coordinates in array to visual board space number
 function mapCoord(mapArray){
-	var val = 0;
 	for (var row = 0; row < 8; row++){
 		var rowMod = row % 2;
 		for (var col = 0; col < 8; col+=2){
 			if (rowMod == 0){
-				mapArray.push([[row, col+1], val]);
+				mapArray.push([row, col+1]);
 			}
 			else {
-				mapArray.push([[row, col], val]);
+				mapArray.push([row, col]);
 			}
-			val++;
 		}
 	}
 	return mapArray;
 }
 
+// Use coordinates to find index of space
+function findIndex(arr, y, x){
+	for (var i = 0; i < arr.length; i++){
+		if (arr[i][0] == y && arr[i][1] == x){
+			return i;
+		}
+	}
+}
+
+// Click function, graphic stuff
 $(".board").on("click", ".black", function(){
-	var id = $(this).attr("id");
-	console.log(id);
+	$(".black").removeClass("selected"); // reset board to black
+	var id = $(this).find("div").attr("id");
+	var checkerPiece = $(this).find("div").attr("class");
+	if (checkerPiece == "redPiece" || checkerPiece == "whitePiece"){
+		$(this).addClass("selected");
+		var openIds = movesAvailable(id);
+		for (var i = 0; i < openIds.length; i++){
+			$("#" + openIds[i]).parent().addClass("selected");
+		}
+	}
+	else {
+		console.log("No piece to move there bud");
+	}
 });
 
-/*
-function selectPiece(){
-	var id = this.id;
-	var x = coordArray[0][0][0];
-	var y = coordArray[0][0][1];
 
+// Receives id of space, returns available spaces
+function movesAvailable(id){
+	var openSpaces = [];
+	var y = coordArray[id][0];
+	var x = coordArray[id][1];
+	var current = window.boardArray[y][x];
+	console.log(current);
+	// check edge cases
+	//make recursive
+
+	if (current.isKing == true){
+		for (var i = -1; i < 2; i+=2){
+			for (var j = -1; j > 2; j+=2){
+				if (window.boardArray[y+i][x+j] == 0){
+					openSpaces.push(findIndex(coordArray, y+i, x+j));
+				}
+			}
+		}
+	}
+	else if (current.team == "Red"){
+		for (var i = -1; i < 2; i+=2){
+			if (window.boardArray[y-1][x+i] == 0){
+				openSpaces.push(findIndex(coordArray, y-1, x+i));
+			}
+		}
+	}
+	else if (current.team == "White"){
+		for (var i = -1; i < 2; i+=2){
+			if (window.boardArray[y+1][x+i] == 0){
+				openSpaces.push(findIndex(coordArray, y+1, x+i));
+			}
+		}
+	}
+	console.log(openSpaces);
+	return openSpaces;
+		//if empty
+		//if same team
+		//if different team
 }
-*/
+
 
 
 
